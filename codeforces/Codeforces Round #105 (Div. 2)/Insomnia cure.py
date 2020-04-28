e3ddd0f85806a1b3810aaab4968b2b73cb21eae0
Sriram Desai <isriramdesai@gmail.com>
2020-04-28 23:02:29 +0530

    Insomnia cure - Codeforces Round #105 (Div. 2)

---
+++ codeforces/Codeforces Round #105 (Div. 2)/Insomnia cure.py
@@ -0,0 +1,13 @@
+k = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+d = int(input())
+
+ct = 0
+
+for i in range(1, d + 1):
+    if i % k == 0 or i % l == 0 or i % m == 0 or i % n == 0:
+        ct += 1
+
+print (ct)
